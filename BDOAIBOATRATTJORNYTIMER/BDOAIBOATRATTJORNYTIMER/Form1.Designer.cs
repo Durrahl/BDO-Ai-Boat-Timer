1366510724b587b14801b5527ef1d2ac25cef8ab
Durrahl <Github@BrentonCullis.tk>
2017-07-24 09:41:08 +0100

    Version 1
    Finished the first version of the program

---
+++ BDOAIBOATRATTJORNYTIMER/BDOAIBOATRATTJORNYTIMER/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace BDOAIBOATRATTJORNYTIMER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BtnTmr1StrtStp = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.lblPercent = new System.Windows.Forms.Label();
+            this.lblRealTime = new System.Windows.Forms.Label();
+            this.TimeValue = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnTmr1StrtStp
+            // 
+            this.BtnTmr1StrtStp.Location = new System.Drawing.Point(12, 80);
+            this.BtnTmr1StrtStp.Name = "BtnTmr1StrtStp";
+            this.BtnTmr1StrtStp.Size = new System.Drawing.Size(272, 23);
+            this.BtnTmr1StrtStp.TabIndex = 0;
+            this.BtnTmr1StrtStp.Text = "Timer Start / Stop";
+            this.BtnTmr1StrtStp.UseVisualStyleBackColor = true;
+            this.BtnTmr1StrtStp.Click += new System.EventHandler(this.BtnTmr1StrtStp_Click);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Location = new System.Drawing.Point(12, 109);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(272, 23);
+            this.BtnReset.TabIndex = 1;
+            this.BtnReset.Text = "Timer Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.AutoSize = true;
+            this.lblPercent.ForeColor = System.Drawing.Color.Black;
+            this.lblPercent.Location = new System.Drawing.Point(86, 24);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(44, 13);
+            this.lblPercent.TabIndex = 2;
+            this.lblPercent.Text = "Percent";
+            // 
+            // lblRealTime
+            // 
+            this.lblRealTime.AutoSize = true;
+            this.lblRealTime.Location = new System.Drawing.Point(107, 49);
+            this.lblRealTime.Name = "lblRealTime";
+            this.lblRealTime.Size = new System.Drawing.Size(55, 13);
+            this.lblRealTime.TabIndex = 3;
+            this.lblRealTime.Text = "Real Time";
+            // 
+            // TimeValue
+            // 
+            this.TimeValue.Location = new System.Drawing.Point(218, 41);
+            this.TimeValue.Name = "TimeValue";
+            this.TimeValue.Size = new System.Drawing.Size(75, 20);
+            this.TimeValue.TabIndex = 4;
+            this.TimeValue.Text = "2454";
+            this.TimeValue.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Settings";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Percentage:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Time remaining:";
+            this.label2.UseMnemonic = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 150);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TimeValue);
+            this.Controls.Add(this.lblRealTime);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.BtnTmr1StrtStp);
+            this.Name = "Form1";
+            this.Text = "Bdo NPC cargo ship travel timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnTmr1StrtStp;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Label lblPercent;
+        private System.Windows.Forms.Label lblRealTime;
+        private System.Windows.Forms.TextBox TimeValue;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BDOAIBOATRATTJORNYTIMER/BDOAIBOATRATTJORNYTIMER/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BDOAIBOATRATTJORNYTIMER
+{
+    
+    public partial class Form1 : Form
+    {
+        public float timeRealVal = 2454;
+        public bool PlyingCnt = true;
+        public float floatTimeSetVal;
+        public float fixer;
+        public float RealTime;
+        public bool Showna = false;
+        public Form1()
+        
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Reset()
+        {
+            
+            timeRealVal = float.Parse(TimeValue.Text);
+            lblRealTime.Text = Convert.ToString(timeRealVal);
+            lblPercent.Text = Convert.ToString((((timeRealVal - floatTimeSetVal) * -1) / fixer) * 100);
+
+
+        }
+
+        private void BtnTmr1StrtStp_Click(object sender, EventArgs e)
+        {
+            
+            if (PlyingCnt == true) {
+                timer1.Enabled = true;
+                PlyingCnt = false;
+            }else
+                 {
+                        timer1.Enabled = false;
+                        PlyingCnt = true; 
+                     }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                    
+            if (timeRealVal == 0)
+            {
+                Reset();
+
+            }else{
+                fixer = float.Parse(TimeValue.Text);
+                timeRealVal = float.Parse(Convert.ToString(Convert.ToDouble(timeRealVal) - 0.01));
+                
+                floatTimeSetVal = float.Parse(TimeValue.Text);
+                
+                lblRealTime.Text = Convert.ToString(timeRealVal);
+
+
+                lblPercent.Text = Convert.ToString((((timeRealVal - floatTimeSetVal) * -1) / fixer)* 100);
+
+
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Showna == false) {
+                TimeValue.Visible = true;
+                Showna = true;
+
+
+            }else{
+
+                TimeValue.Visible = false;
+                Showna = false;
+
+            }
+
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+    }
+}
